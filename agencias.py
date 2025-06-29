6dbce4c0985aa3c27feea6a56181144b8fb98d58
opropriofabricio <fabricio.mh@outlook.com>
2025-06-28 22:15:02 -0300

    add files

---
+++ agencias.py
@@ -0,0 +1,85 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+import pandas as pd
+from scraping import Scraping
+import numpy as np
+
+
+
+
+geral = []
+ag_aux = []
+colunas = ['Nome', 'Endereco', 'Cep - UF', 'Telefone', 'Numero CGC', 'Horario de Atendimento']
+url = "https://www.caixa.gov.br/atendimento/Paginas/encontre-a-caixa.aspx"
+
+
+with Scraping(url=url) as driver:
+
+    try:
+        # submit cookie
+        driver.find_element(By.ID, 'adopt-accept-all-button').click()
+
+        # Primeiro cammpo de selecao: Tipo de Atendimentos
+        classe_atendimento = driver.find_element(By.CLASS_NAME, 'select-d')
+        tipo_atendimento = Select(classe_atendimento)
+        # 1 = Agencias
+        tipo_atendimento.select_by_value("1")
+
+        time.sleep(3)
+        # Segundo campo: Unidade Federativa - UF
+        uf = driver.find_element(By.ID, 'ctl00_ctl61_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_ddlUf')
+        regiao = Select(uf)
+        # RJ = Rio de Janeiro
+        regiao.select_by_value("RJ")
+
+        time.sleep(3)
+        # Numero de cidades do rio de janeiro
+        class_cont = driver.find_element(By.ID, 'ctl00_ctl61_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_ddlCidade')
+        # lista de elementos: cidades do rio de janeiro
+        total_agencias = len(Select(class_cont).options)
+        
+
+        for i, item in enumerate(range(total_agencias)):
+            #print(agencias.options[item].text)
+            municipio = driver.find_element(By.ID, 'ctl00_ctl61_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_ddlCidade')
+            agencias = Select(municipio)
+
+            # botão de pesquisar
+            time.sleep(3)
+            btn = driver.find_element(By.ID, 'ctl00_ctl61_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_btnBuscar')
+
+            # Seleciona um municipio e pesquisa
+            agencias.options[item].click()
+            
+            btn.click()
+            time.sleep(3)
+
+            # Resultado da Pesuisa pode ser dessa classe 
+            resultado = driver.find_element(By.CLASS_NAME, 'colsm-7')
+            resultado2 = driver.find_elements(By.CLASS_NAME, 'resultado-busca-item')
+            res = resultado.text
+
+            match res:
+
+                case 'Nenhum ponto de atendimento foi localizado com os dados informados.':
+                    print(f'Não há dados para essa opção')
+                
+                case '':
+                    print(f'Opcao não é valida')
+                case _:
+                    for item in resultado2:
+                        ax = item.text.split('\n')
+                        array_1x6 = np.array(ax)
+                        geral.append(pd.DataFrame([array_1x6], columns=colunas))
+            print(f'\n\n: vez: {i}')
+
+    finally:
+        driver.quit()
+
+
+data = pd.concat(geral)
+
+data.to_csv('Agencias_Estado_RJ.csv')
+
+
---
+++ scraping.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from rich.console import Console
+
+
+
+
+
+class Scraping:
+    
+    def __init__(self, url:str):
+        self.console = Console()
+        self.url = url
+        self.options = chrome_options = Options()
+        self.options.add_argument("--headless=new")
+        self.options.add_argument("--disable-gpu")
+        
+        
+    def __enter__(self):
+        self.service = Service(ChromeDriverManager().install())
+        self.scraping = webdriver.Chrome(service=self.service, options=self.options)
+        self.scraping.implicitly_wait(3)
+        self.scraping.get(self.url)
+        return self.scraping
+
+    def __exit__(self, exc_type, exc_value, exc_trace):
+        if exc_type != None:
+            self.console.print(f"Erro do tipo:{exc_type} | {exc_value}, {exc_trace}", style="red")
+        else:    
+            self.scraping.quit()
+
+
+
+
+#with Scraping(url='https://pleno.news/') as driver:
+    #print('Executrabndoo...\n\n\n')
+    #print(driver.title)
